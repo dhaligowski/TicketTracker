1def3db24f46b5f8b254c7ab87a231f559b3d32c
DHaligowski <dhaligowski@gmail.com>
2021-03-03 12:21:55 -0500

    Optimize assets with sharp-cli.

--- app/api/client.js
+++ app/api/client.js
@@ -2,8 +2,8 @@ import { create } from "apisauce";
 import authStorage from "../auth/storage";
 
 const apiClient = create({
-  baseURL: "http://192.168.1.65:9000/api",
-  //baseURL: "https://afternoon-spire-84745.herokuapp.com/api",
+  //baseURL: "http://192.168.1.65:9000/api",
+  baseURL: "https://afternoon-spire-84745.herokuapp.com/api",
 });
 
 apiClient.addAsyncRequestTransform(async (request) => {
